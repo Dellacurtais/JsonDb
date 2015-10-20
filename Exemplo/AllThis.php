1a6913e6c3a884312a0d5d5b9c1e255a0d2f5665
Maicon <maiconphpnet@gmail.com>
2015-10-20 19:01:44 -0200

    Exemplo Update
    Exemplo Update

--- Exemplo/AllThis.php
+++ Exemplo/AllThis.php
@@ -156,3 +156,31 @@ $Data = $User->findByNome("Felipe")->toArray();
 echo "<pre>";
 print_r($Data);
 echo "</pre>";
+
+
+//http://jsondb.inclouds.com.br/index.php/Page/exportar-para-cvs
+
+//////////////////////Export All
+
+//$User = new ModelJdb_Users();
+
+/**
+* $Delimiter is Optional 
+* Ex: $User->getCvs(";"); 
+* Ex: $User->getCvs(",");
+*/
+
+//$User->getCvs();
+
+//////////////////////Export Results
+
+//$User = new ModelJdb_Users();
+//$User->findByNome("Felipe");
+
+/**
+* $Delimiter is Optional 
+* Ex: $User->getCvs(";"); 
+* Ex: $User->getCvs(",");
+*/
+
+//$User->getCvs();
